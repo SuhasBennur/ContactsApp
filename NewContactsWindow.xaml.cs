2a7c52a0703e3b0d3a80cd8d506086f9681e17db
Suhas S Bennur <suhasbennur@gmail.com>
2022-01-28 10:11:34 +0530

    Add project files.

---
+++ NewContactsWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ContactsApp.classes;
+using SQLite;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactsWindow.xaml
+    /// </summary>
+    public partial class NewContactsWindow : Window
+    {
+        public NewContactsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = NameBox.Text,
+                Email = EmailBox.Text,
+                Phone = PhoneBox.Text,
+            };
+            string dbName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string dbPath = System.IO.Path.Combine(folderPath, dbName);
+
+            using (SQLiteConnection con = new SQLiteConnection(dbPath))
+            {
+                con.CreateTable<Contact>();
+                con.Insert(contact);
+            }
+
+            Close();
+        }
+    }
+}
---
+++ classes/Contact.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.classes
+{
+    public class Contact
+    {
+        [PrimaryKey,AutoIncrement,Unique]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [MaxLength(10)]
+        public string Phone { get; set; }
+    }
+}
